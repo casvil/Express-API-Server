8260fe66d307ead0f8f8c42cb76096dde480d9ea
David Casanellas <davidcasanell@gmail.com>
2018-06-24 22:43:32 +0200

    fix: cors commented - weird behaviour postman/chrome

--- index.js
+++ index.js
@@ -6,11 +6,13 @@ const morgan = require('morgan');
 const app = express();
 const router = require('./router');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 // DB Setup
 mongoose.connect('mongodb://localhost:27017/auth');
 
 // App Setup
+// app.use(cors);
 app.use(morgan('combined')); // login framework middleware
 app.use(bodyParser.json({ type: '*/*' })); // parse requests into json middleware
 router(app);
