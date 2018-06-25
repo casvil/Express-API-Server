a9845fc0332791cae757d1026d933d6d30fbfe6b
David Casanellas <davidcasanell@gmail.com>
2018-06-25 22:15:19 +0200

    chore: passport auth

--- services/passport.js
+++ services/passport.js
@@ -3,3 +3,24 @@ const User = require('../models/user');
 const config = require('../config.js');
 const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
+
+// Set options for JWT strategy
+const jwtOptions = {
+  jwtFromRequest: ExtractJwt.fromHeader('authorization'),
+  secretOrKey: config.secret
+};
+
+// Create JWT strategy
+const jwtLogin = new JwtStrategy(jwtOptions, (payload, done) => {
+  // Check if user ID exists in db, otherwise
+  // call 'done' without a user object
+  User.findById(payload.sub, (err, user) => {
+    if (err) return done(err, false);
+
+    if (user) done(null, user);
+    else done(null, false);
+  });
+});
+
+// Tell passport to use this strategy
+passport.use(JwtStrategy);
