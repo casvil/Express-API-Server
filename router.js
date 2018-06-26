6424e8fde3468e893c24849d757358620e51a2d9
David Casanellas <davidcasanell@gmail.com>
2018-06-26 14:06:32 +0200

    feat: POST /signin w/ requireSignin and Authentication.signin

--- router.js
+++ router.js
@@ -5,9 +5,11 @@ const Authentication = require('./controllers/authentication');
 const Recipe = require('./controllers/recipe');
 
 const requireAuth = passport.authenticate('jwt', { session: false });
+const requireSignin = passport.authenticate('local', { session: false });
 
 module.exports = function(app) {
   app.post('/signup', Authentication.signup);
+  app.post('/signin', requireSignin, Authentication.signin);
   app.post('/recipe', requireAuth, Recipe.addEntry);
   app.get('/recipe', Recipe.getAllEntries);
 };
